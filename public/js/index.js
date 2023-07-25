0cdf1a8bf67d8b011d599fc9b144488af4df8faa
gudu <3072953029@qq.com>
2023-07-25 23:04:59 +0800

    优化柠檬学堂平台学期选择显示不全问题

--- public/js/index.js
+++ public/js/index.js
@@ -181,7 +181,6 @@ function getSemester(type) {
         }
 
         $.each(arr, function (index, item) {
-            console.log(item);
             var option = document.createElement('option');
             option.innerText = '第' + item.term + '学期';
             option.selected = item.isCurrentTerm;
